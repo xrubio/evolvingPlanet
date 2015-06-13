f4db9aae87e50a6637c7826e8bf540dc6820a900
Xavier Rubio <xrubio@Xaviers-MacBook-Pro.local>
2015-06-13 16:29:27 -0700

    updatd version

--- AppController.mm
+++ AppController.mm
@@ -23,7 +23,7 @@
  ****************************************************************************/
 
 #import "AppController.h"
-#import "CCEAGLView.h"
+#import "platform/ios/CCEAGLView-ios.h"
 #import "cocos2d.h"
 #import "AppDelegate.h"
 #import "RootViewController.h"
@@ -38,6 +38,9 @@ static AppDelegate s_sharedApplication;
 
 - (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
 {
+    cocos2d::Application *app = cocos2d::Application::getInstance();
+    app->initGLContextAttrs();
+    cocos2d::GLViewImpl::convertAttrs();
 
     // Override point for customization after application launch.
 
@@ -45,19 +48,21 @@ static AppDelegate s_sharedApplication;
     window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 
     // Init the CCEAGLView
-    CCEAGLView* eaglView = [CCEAGLView viewWithFrame:[window bounds]
-                                         pixelFormat:kEAGLColorFormatRGBA8
-                                         depthFormat:GL_DEPTH24_STENCIL8_OES
-                                  preserveBackbuffer:NO
-                                          sharegroup:nil
-                                       multiSampling:NO
-                                     numberOfSamples:0];
+    CCEAGLView *eaglView = [CCEAGLView viewWithFrame: [window bounds]
+                                         pixelFormat: (NSString*)cocos2d::GLViewImpl::_pixelFormat
+                                         depthFormat: cocos2d::GLViewImpl::_depthFormat
+                                  preserveBackbuffer: NO
+                                          sharegroup: nil
+                                       multiSampling: NO
+                                     numberOfSamples: 0 ];
+    
+    [eaglView setMultipleTouchEnabled:YES]; // Multitouch
 
     // Use RootViewController manage CCEAGLView
     _viewController = [[RootViewController alloc] initWithNibName:nil bundle:nil];
+    // no full screen
     //_viewController.wantsFullScreenLayout = YES;
     _viewController.view = eaglView;
-    [eaglView setMultipleTouchEnabled:YES]; // Multitouch
 
     // Set RootViewController to window
     if ([[UIDevice currentDevice].systemVersion floatValue] < 6.0) {
@@ -73,10 +78,10 @@ static AppDelegate s_sharedApplication;
     [[UIApplication sharedApplication] setStatusBarHidden:true];
 
     // IMPORTANT: Setting the GLView should be done after creating the RootViewController
-    cocos2d::GLView* glview = cocos2d::GLView::createWithEAGLView(eaglView);
+    cocos2d::GLView* glview = cocos2d::GLViewImpl::createWithEAGLView(eaglView);
     cocos2d::Director::getInstance()->setOpenGLView(glview);
 
-    cocos2d::Application::getInstance()->run();
+    app->run();
 
     return YES;
 }
