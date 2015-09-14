0e8ab0f6e8dec6598c9ff4306774b8709ea4f01a
Guillem Laborda <badulaka@gmail.com>
2015-09-14 16:59:51 +0200

    fixes cocos2dx 3.8 crash

--- AppController.mm
+++ AppController.mm
@@ -36,16 +36,16 @@
 // cocos2d application instance
 static AppDelegate s_sharedApplication;
 
-- (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
-{
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
+
     cocos2d::Application *app = cocos2d::Application::getInstance();
     app->initGLContextAttrs();
-    cocos2d::GLViewImpl::convertAttrs();
+    //cocos2d::GLViewImpl::convertAttrs();
 
     // Override point for customization after application launch.
 
     // Add the view controller's view to the window and display.
-    window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    window = [[UIWindow alloc] initWithFrame: [[UIScreen mainScreen] bounds]];
 
     // Init the CCEAGLView
     CCEAGLView *eaglView = [CCEAGLView viewWithFrame: [window bounds]
@@ -56,19 +56,22 @@ static AppDelegate s_sharedApplication;
                                        multiSampling: NO
                                      numberOfSamples: 0 ];
     
-    [eaglView setMultipleTouchEnabled:YES]; // Multitouch
+    // Enable or disable multiple touches
+    [eaglView setMultipleTouchEnabled:YES];
 
-    // Use RootViewController manage CCEAGLView
+    // Use RootViewController manage CCEAGLView 
     _viewController = [[RootViewController alloc] initWithNibName:nil bundle:nil];
-    // no full screen
-    //_viewController.wantsFullScreenLayout = YES;
+   // _viewController.wantsFullScreenLayout = YES;
     _viewController.view = eaglView;
 
     // Set RootViewController to window
-    if ([[UIDevice currentDevice].systemVersion floatValue] < 6.0) {
+    if ( [[UIDevice currentDevice].systemVersion floatValue] < 6.0)
+    {
         // warning: addSubView doesn't work on iOS6
-        [window addSubview:_viewController.view];
-    } else {
+        [window addSubview: _viewController.view];
+    }
+    else
+    {
         // use this method on ios6
         [window setRootViewController:_viewController];
     }
@@ -78,7 +81,7 @@ static AppDelegate s_sharedApplication;
     [[UIApplication sharedApplication] setStatusBarHidden:true];
 
     // IMPORTANT: Setting the GLView should be done after creating the RootViewController
-    cocos2d::GLView* glview = cocos2d::GLViewImpl::createWithEAGLView(eaglView);
+    cocos2d::GLView *glview = cocos2d::GLViewImpl::createWithEAGLView(eaglView);
     cocos2d::Director::getInstance()->setOpenGLView(glview);
 
     app->run();
@@ -86,27 +89,25 @@ static AppDelegate s_sharedApplication;
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication*)application
-{
+
+- (void)applicationWillResignActive:(UIApplication *)application {
     /*
      Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
      Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
      */
-    //We don't need to call this method any more. It will interupt user defined game pause&resume logic
+     //We don't need to call this method any more. It will interupt user defined game pause&resume logic
     /* cocos2d::Director::getInstance()->pause(); */
 }
 
-- (void)applicationDidBecomeActive:(UIApplication*)application
-{
+- (void)applicationDidBecomeActive:(UIApplication *)application {
     /*
      Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
      */
-    //We don't need to call this method any more. It will interupt user defined game pause&resume logic
+     //We don't need to call this method any more. It will interupt user defined game pause&resume logic
     /* cocos2d::Director::getInstance()->resume(); */
 }
 
-- (void)applicationDidEnterBackground:(UIApplication*)application
-{
+- (void)applicationDidEnterBackground:(UIApplication *)application {
     /*
      Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
      If your application supports background execution, called instead of applicationWillTerminate: when the user quits.
@@ -114,36 +115,35 @@ static AppDelegate s_sharedApplication;
     cocos2d::Application::getInstance()->applicationDidEnterBackground();
 }
 
-- (void)applicationWillEnterForeground:(UIApplication*)application
-{
+- (void)applicationWillEnterForeground:(UIApplication *)application {
     /*
      Called as part of  transition from the background to the inactive state: here you can undo many of the changes made on entering the background.
      */
     cocos2d::Application::getInstance()->applicationWillEnterForeground();
 }
 
-- (void)applicationWillTerminate:(UIApplication*)application
-{
+- (void)applicationWillTerminate:(UIApplication *)application {
     /*
      Called when the application is about to terminate.
      See also applicationDidEnterBackground:.
      */
 }
 
+
 #pragma mark -
 #pragma mark Memory management
 
-- (void)applicationDidReceiveMemoryWarning:(UIApplication*)application
-{
+- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application {
     /*
      Free up as much memory as possible by purging cached data objects that can be recreated (or reloaded from disk) later.
      */
 }
 
-- (void)dealloc
-{
+
+- (void)dealloc {
     [window release];
     [super dealloc];
 }
+
 
 @end
