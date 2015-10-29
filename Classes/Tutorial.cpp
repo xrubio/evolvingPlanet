beb7b8b60091afb9ca72383d71fe2e2d284b7da3
xrubio <xrubio@gmail.com>
2015-10-29 17:56:15 +0100

    small issue with double tap in tutorial

--- Classes/Tutorial.cpp
+++ Classes/Tutorial.cpp
@@ -66,8 +66,6 @@ void Tutorial::loadMessagesForLevel(const pugi::xml_node & node)
             std::string text = LocalizedString::create(message.attribute("text").value(), "tutorial");
             std::string image = message.attribute("image").value();
             Message * newMessage = 0;
-            // add a dummy message if spot due to double tap (2 taps are added so we need to "swallow" a dummy one to prevent closing the next message.)
-            bool addDummy = false;
             if(trigger=="next")
             { 
                 newMessage = new MessageNext(text, xPos, yPos, lineBreak, image);
@@ -87,10 +85,6 @@ void Tutorial::loadMessagesForLevel(const pugi::xml_node & node)
                     std::string button = message.attribute("button").value();
                     newMessage->setPostConditionButtonTap(button);
                 }
-                else if(condition=="spot")
-                {
-                    addDummy = true;
-                }
             }
 
             // add spots (if any)
@@ -105,11 +99,6 @@ void Tutorial::loadMessagesForLevel(const pugi::xml_node & node)
             }
 
             _messages.push_back(newMessage);
-            if(addDummy)
-            {
-                Message * dummy = new MessageNext("", xPos, yPos, lineBreak, "");
-                _messages.push_back(dummy);
-            }
             message = message.next_sibling("message");
         }
     }
