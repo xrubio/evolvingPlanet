9bab4cb29b9ab492425ff2ef66b9466a9d9c5b7d
Xavier Rubio <xrubio@gmail.com>
2015-12-27 11:44:43 +0100

    updated android config

--- Application.mk
+++ Application.mk
@@ -2,7 +2,8 @@ APP_STL := c++_shared
 
 APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-char
 APP_LDFLAGS := -latomic
-
+APP_ABI :=  armeabi-v7a
+APP_PLATFORM := android-19
 
 ifeq ($(NDK_DEBUG),1)
   APP_CPPFLAGS += -DCOCOS2D_DEBUG=1
@@ -11,3 +12,4 @@ else
   APP_CPPFLAGS += -DNDEBUG
   APP_OPTIM := release
 endif
+
