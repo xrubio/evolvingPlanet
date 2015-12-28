c79c02573e5f5aa9998c19f2e03fe5e14024cf64
Guillem Laborda <badulaka@gmail.com>
2015-12-28 19:22:54 +0100

    animation attempt 2

--- Classes/UIIntro.cpp
+++ Classes/UIIntro.cpp
@@ -122,7 +122,7 @@ void UIIntro::update(float delta)
     
 // video player only for ios/android    
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID || CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    if (((VideoPlayer*)this->getChildByName("video")) != nullptr and((VideoPlayer*)this->getChildByName("video"))->isPlaying() == false)
+    if (((VideoPlayer*)this->getChildByName("video")) != nullptr and ((VideoPlayer*)this->getChildByName("video"))->isPlaying() == false)
     {
         this->removeChild(((VideoPlayer*)this->getChildByName("video")));
         /*_eventDispatcher->removeEventListener(_listener);
@@ -131,28 +131,50 @@ void UIIntro::update(float delta)
         auto camaraBack = Sprite::create("misc/CamaraBack.jpg");
         camaraBack->setPosition(Vec2(Director::getInstance()->getVisibleSize().width / 2, Director::getInstance()->getVisibleSize().height / 2));
         camaraBack->setScale(GameData::getInstance()->getRaHConversion());
-        this->addChild(camaraBack);
+        this->addChild(camaraBack, 0);
+        
+        auto camaraHexagon = Sprite::create("misc/CamaraHexagon.png");
+        camaraHexagon->setPosition(Vec2(9 * Director::getInstance()->getVisibleSize().width / 19,  6 * Director::getInstance()->getVisibleSize().height / 11));
+        this->addChild(camaraHexagon, 1);
+        camaraHexagon->runAction(Sequence::create(Spawn::create(Repeat::create(RotateBy::create(0.3, -270), 3), ScaleTo::create(0.9, 1.5), NULL),Spawn::create(Repeat::create(RotateBy::create(0.25, 360), 4), ScaleTo::create(1.0, 0.5), NULL), FadeOut::create(0.5), NULL));
         
         auto camaraFront = ProgressTimer::create(Sprite::create("misc/CamaraFront.png"));
         camaraFront->setPosition(Vec2(Director::getInstance()->getVisibleSize().width / 2, Director::getInstance()->getVisibleSize().height / 2));
         camaraFront->setPercentage(0.0);
         camaraFront->setName("camaraFront");
         camaraFront->setScale(GameData::getInstance()->getRaHConversion());
-        this->addChild(camaraFront);
+        this->addChild(camaraFront, 1);
         
-        ProgressFromTo* radialTimer = ProgressFromTo::create(2.5, 0, 100);
+        auto radialTimer = ProgressFromTo::create(3.0, 0, 66);
+        auto radialTimer2 = ProgressFromTo::create(0.2, 66, 90);
         camaraFront->setMidpoint(Vec2(0, 0));
         camaraFront->setBarChangeRate(Vec2(0, 1));
         camaraFront->setType(ProgressTimer::Type::BAR);
-        camaraFront->runAction(radialTimer);
+        camaraFront->runAction(Sequence::create(DelayTime::create(1.3), radialTimer, radialTimer2, NULL));
+    
+        auto particleSmoke = ParticleSystemQuad::create("audio/CamaraParticle.plist");
+        particleSmoke->setPosition(Vec2(Director::getInstance()->getVisibleSize().width / 2, 0.0));
+        auto moveParticle = MoveTo::create(5.0, Vec2(Director::getInstance()->getVisibleSize().width / 2, Director::getInstance()->getVisibleSize().height / 4));
+        this->addChild(particleSmoke, 5);
+        particleSmoke->runAction(moveParticle);
+        
+        auto camaraDoor = Sprite::create("misc/CamaraDoor.png");
+        camaraDoor->setPosition(Vec2(Director::getInstance()->getVisibleSize().width / 2, 0));//5*Director::getInstance()->getVisibleSize().height/24));
+        camaraDoor->setOpacity(0);
+        camaraDoor->setAnchorPoint(Vec2(0.5, 0.66));
+        camaraDoor->setName("camaraDoor");
+        this->addChild(camaraDoor, 3);
+        auto moveDoor = Spawn::create(FadeIn::create(1.5), MoveTo::create(3.0, Vec2(Director::getInstance()->getVisibleSize().width / 2, Director::getInstance()->getVisibleSize().height * 0.66)), NULL);
+        camaraDoor->runAction(Sequence::create(DelayTime::create(1.3), moveDoor, NULL));
         
     }
     
-    if (this->getChildByName("camaraFront") != nullptr and this->getChildByName("camaraFront")->getNumberOfRunningActions()==0)
+    if (this->getChildByName("camaraFront") != nullptr and this->getChildByName("camaraFront")->getNumberOfRunningActions()==0
+        and this->getChildByName("camaraDoor") != nullptr and this->getChildByName("camaraDoor")->getNumberOfRunningActions()==0)
     {
         _eventDispatcher->removeEventListener(_listener);
         auto scene = UIMainMenu::createScene();
-        Director::getInstance()->replaceScene(scene);
+        Director::getInstance()->replaceScene(TransitionFade::create(0.5, scene));
     }
     
 # else
