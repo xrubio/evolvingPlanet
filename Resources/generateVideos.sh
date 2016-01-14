445299923d89a0c73112b768998d5cd8923f9aa4
Xavier Rubio <xrubio@gmail.com>
2016-01-14 08:43:06 +0100

    intro video format compatible with android devices

--- Resources/generateVideos.sh
+++ Resources/generateVideos.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-avconv -i data/logo.mp4 -profile:v baseline -level 30 -s 2048x1536 -c:v h264 -c:a copy  -acodec libmp3lame -ac 2 01_hd/misc/logo.mp4
-avconv -i data/logo.mp4 -profile:v baseline -level 30 -s 1536x1152 -c:v h264 -c:a copy  -acodec libmp3lame -ac 2 02_threeQuarters/misc/logo.mp4
-avconv -i data/logo.mp4 -profile:v baseline -level 30 -s 1280x960 -c:v h264 -c:a copy  -acodec libmp3lame -ac 2 03_fifthEights/misc/logo.mp4
-avconv -i data/logo.mp4 -profile:v baseline -level 30 -s 1024x768 -c:v h264 -c:a copy  -acodec libmp3lame -ac 2 04_half/misc/logo.mp4
+avconv -i data/logo.mp4 -vcodec libx264 -preset medium -vprofile baseline -acodec aac -strict experimental -r 24 -b 255k -ar 44100 -s 2048x1536 -ab 59k -y 01_hd/misc/logo.mp4
+avconv -i data/logo.mp4 -vcodec libx264 -preset medium -vprofile baseline -acodec aac -strict experimental -r 24 -b 255k -ar 44100 -s 1536x1152 -ab 59k -y 02_threeQuarters/misc/logo.mp4
+avconv -i data/logo.mp4 -vcodec libx264 -preset medium -vprofile baseline -acodec aac -strict experimental -r 24 -b 255k -ar 44100 -s 1280x960 -ab 59k -y 03_fifthEights/misc/logo.mp4
+avconv -i data/logo.mp4 -vcodec libx264 -preset medium -vprofile baseline -acodec aac -strict experimental -r 24 -b 255k -ar 44100 -s 1024x768 -ab 59k -y 04_half/misc/logo.mp4
+
 
